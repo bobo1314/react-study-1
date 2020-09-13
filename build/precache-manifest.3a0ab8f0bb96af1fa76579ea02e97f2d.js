b7ed546197c04c15d9ecb34c3cf01f0a74bee577
zhangbo <1174839512@qq.com>
2020-09-13 12:23:26 +0800

    提交build

---
+++ build/precache-manifest.3a0ab8f0bb96af1fa76579ea02e97f2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec3e5ba1f8fde0e2f4cc7c28c217c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2500bf0e7fe0087d7d95",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca1ac9c674e6fcf84b02",
+    "url": "/static/js/2.823e42a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.823e42a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2500bf0e7fe0087d7d95",
+    "url": "/static/js/main.ca5b9154.chunk.js"
+  },
+  {
+    "revision": "0fe42fad118911a3ed1e",
+    "url": "/static/js/runtime-main.75ca3cac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
